fae06aaf3df7ac2fa62a8c9d9d69aa50bd77cb33
윤재상 <apple01644@hotmail.com>
2019-10-16 08:18:28 +0900

    Backup

---
+++ swipe break block/Python/Model.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+
+class DDQN_Model:
+    def __init__(self):
+        tf.Dense(128)
+
--- swipe break block/python/ENV.py
+++ swipe break block/python/ENV.py
@@ -1,34 +1,40 @@
 from Game import SBBGame
+from Model import DDQN_Model
 import random, time
 
 now_state = {}
-act = -1
+act = 0.5
 
 epsilon = 1
-time_step = 0
+game_attempt = 0
 
 EPSILON_DECREASE_PER_GAME = 1e-4
+EPOCH = 100000
 
-
+brain = DDQN_Model()
 
 game = SBBGame()
 #game.ui = False
 game.user = False
 game.start()
 
-for _ in range(100000):
+for _ in range(EPOCH):
     while game.dqn_state == None: time.sleep(0.01)
     state = game.dqn_state
-    
     game.dqn_state = None
-    
+
+    #set action from state
+    game.guide_line = act
     if epsilon > random.random():
         game.setACT(random.random())
     else:
-        game.setACT(0.5)
+        game.setACT(act)
         
     while game.dqn_data == None: time.sleep(0.01)
     result = game.dqn_data
+    if result[2] == 1:
+        game_attempt += 1
+        pass #died
     
     game.dqn_data = None
 
